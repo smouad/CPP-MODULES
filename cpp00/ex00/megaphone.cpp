95cbfc1c4fd3fcf58691574dd0ba849d00529738
Mouad Sodor <sodormouad@gmail.com>
2023-07-23 14:45:34 +0100

    edit

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,19 +6,11 @@
 /*   By: msodor <msodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 12:47:15 by msodor            #+#    #+#             */
-/*   Updated: 2023/07/23 13:18:03 by msodor           ###   ########.fr       */
+/*   Updated: 2023/07/23 14:44:59 by msodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-char	to_upper(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c - 32);
-	else
-		return (c);
-}
 
 int main(int ac, char **av)
 {
@@ -27,7 +19,7 @@ int main(int ac, char **av)
 	else{
 		for (int i = 1; av[i]; i++){
 			for (int j = 0; av[i][j]; j++)
-				std::cout << to_upper(av[i][j]);
+				std::cout << (char)toupper(av[i][j]);
 		}
 	}
 	std::cout << std::endl;
